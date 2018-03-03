8349f9c995246d60abf7098b8b053486fe51f6d7
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-03-03 23:15:06 +0100

    An introduction to stream

---
+++ java8-exercise03/src/main/java/com/abouzidi/javaeight/MobileService.java
@@ -0,0 +1,11 @@
+package com.abouzidi.javaeight;
+
+import java.util.List;
+
+public interface MobileService {
+
+	public List<Mobile> findSamSungMobiles(List<Mobile> mobiles);
+
+	public List<Mobile> findCheaperMobiles(List<Mobile> mobiles);
+
+}
---
+++ java8-exercise03/src/main/java/com/abouzidi/javaeight/MobileServiceAfter.java
@@ -0,0 +1,18 @@
+package com.abouzidi.javaeight;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MobileServiceAfter implements MobileService {
+
+	@Override
+	public List<Mobile> findSamSungMobiles(List<Mobile> mobiles) {
+		return mobiles.stream().filter((Mobile m) -> MobileType.SAMSUNG.equals(m.getMobileType()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public List<Mobile> findCheaperMobiles(List<Mobile> mobiles) {
+		return mobiles.stream().filter((Mobile m) -> m.getPrice() <= 100).collect(Collectors.toList());
+	}
+}
