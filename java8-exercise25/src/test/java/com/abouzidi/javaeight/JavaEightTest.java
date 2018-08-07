db3407aeb925371a69b6b0679e944fcdf897c5b1
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-08-07 13:16:56 +0100

    collectors:Grouping

---
+++ java8-exercise25/src/test/java/com/abouzidi/javaeight/JavaEightTest.java
@@ -0,0 +1,36 @@
+package com.abouzidi.javaeight;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavaEightTest {
+
+	
+	private final Logger LOG = Logger.getLogger(JavaEightTest.class.getName());
+	private List<Mobile> mobiles = new ArrayList<>();
+	
+	@Before
+	public void setUp() throws Exception {
+		Mobile MobileOne = new Mobile(MobileType.IPHONE, 123.3f);
+		Mobile MobileTwo = new Mobile(MobileType.IPHONE, 63.5f);
+		Mobile MobileThree = new Mobile(MobileType.WINDOWSPHONE, 50f);
+		Mobile MobileFour = new Mobile(MobileType.SAMSUNG, 50f);
+		mobiles.addAll(Arrays.asList(MobileOne, MobileTwo, MobileThree, MobileFour));
+	}
+	
+	@Test
+	public void test() {
+      Map<MobileType,List<Mobile>> mobilesByType=mobiles.stream().collect(Collectors.groupingBy(Mobile::getMobileType));
+      LOG.info("mobilesByType :"+mobilesByType);
+	}
+}
